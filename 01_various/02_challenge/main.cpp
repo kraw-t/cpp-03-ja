491540d0437d74f315831a8cec23957a09269083
tanaka <152840606+kraw-t@users.noreply.github.com>
2024-04-05 20:38:33 +0900

    report: 01_various/02_challenge

--- 01_various/02_challenge/main.cpp
+++ 01_various/02_challenge/main.cpp
@@ -1,1 +1,12 @@
 // コードを入力してください
+#include <iostream>
+#include "templates.hpp"
+
+int main() {
+  std::cout << sum(1, 2, 3, 4, 5) << std::endl;
+  std::cout << sum(1, 2, 3) << std::endl;
+  std::cout << sum(-1, 2, 3, 4) << std::endl;
+  std::cout << sum(0, 1, 2, -3, 4, -5) << std::endl;
+
+  return 0;
+}
--- 01_various/02_challenge/templates.hpp
+++ 01_various/02_challenge/templates.hpp
@@ -1,1 +1,17 @@
-// コードを入力してください
+#pragma once
+
+// sizeof...(Args)のif分をなくして、↓のベースケースを有効にするのでも良い様子
+// int sum() {
+//   return 0;
+// }
+
+template<typename...Args>
+int sum(const int value, const Args&... args) {
+  int ans {0};
+  if constexpr(sizeof...(Args) > 0) {
+    ans = value + sum(args...);
+  } else {
+    ans = value;
+  }
+  return ans;
+}
