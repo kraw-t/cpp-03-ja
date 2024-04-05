b7a5fb95dd37059d71a231e603ed5507ceb8eea5
tanaka <152840606+kraw-t@users.noreply.github.com>
2024-04-05 20:44:45 +0900

    report: 01_various/03_challenge

--- 01_various/03_challenge/templates.hpp
+++ 01_various/03_challenge/templates.hpp
@@ -1,1 +1,13 @@
 // コードを入力してください
+#pragma once
+
+// ベースケース
+bool allTrue() {
+  return true;
+}
+
+template<typename...Args>
+bool allTrue(bool arg1, const Args&... args) {
+  bool ans = (arg1 && allTrue(args...));
+  return ans;
+}
