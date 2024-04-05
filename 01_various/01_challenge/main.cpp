62ebd38bc16f1e20cee8f87e16ae2e68d16f7b5f
tanaka <152840606+kraw-t@users.noreply.github.com>
2024-04-05 20:14:50 +0900

    report: 01_various/01_challenge

--- 01_various/01_challenge/main.cpp
+++ 01_various/01_challenge/main.cpp
@@ -1,1 +1,15 @@
-// コードを入力してください
+#include <thread>
+#include <chrono>
+#include "timer.hpp"
+
+int main() {
+  Timer* p_timer1 = new Timer();
+  Timer* p_timer2 = new Timer();
+
+  delete p_timer1;
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  delete p_timer2;
+
+  return 0;
+}
--- 01_various/01_challenge/timer.cpp
+++ 01_various/01_challenge/timer.cpp
@@ -1,1 +1,10 @@
-// コードを入力してください
+#include <iostream>
+#include "timer.hpp"
+
+Timer::Timer() : generated_time(std::chrono::high_resolution_clock::now()) {}
+
+Timer::~Timer() {
+  std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+  double elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(now - this->generated_time).count();
+  std::cout << elapsed_time << std::endl;
+}
